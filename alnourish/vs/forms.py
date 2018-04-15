62777eb48d7fc84f5b5cd4deb1c49e015c61f8b2
Ackshay <ackshayjain@gmail.com>
2018-04-15 18:34:46 +0530

    fixed contact

--- alnourish/vs/forms.py
+++ alnourish/vs/forms.py
@@ -1,11 +1,8 @@
 from django import forms
 
+
 class ContactForm(forms.Form):
-
-    name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class':'form-control'}))
-    email = forms.CharField(widget = forms.TextInput(attrs={'class':'form-control'}))
-    phone = forms.IntegerField(widget = forms.TextInput(attrs={'class':'form-control'}))
-    comments = forms.CharField(widget = forms.TextInput(attrs={'class':'form-control'}))
-
-
-
+    name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    phone = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    comments = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
--- alnourish/vs/models.py
+++ alnourish/vs/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 #
 # Create your models here.
 class Contact(models.Model):
-
     id = models.AutoField(primary_key=True)
     # username = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
